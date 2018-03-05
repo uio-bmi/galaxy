adf7b3ce886f9afdf4b8bc2eada60b363c9e6ae6
Ivar Grytten <ivar.grytten@gmail.com>
2018-03-05 09:57:09 +0100

    Dev

--- tools/graph_based_analysis/graph_peak_caller_wrapper.sh
+++ tools/graph_based_analysis/graph_peak_caller_wrapper.sh
@@ -1,8 +1,6 @@
 source activate py36
 alias graph_peak_caller="~/.conda/envs/py36/bin/python3.6 ~/graph_peak_caller/graph_peak_caller/command_line_interface.py"
 
-exit 0
-
 genome=$1
 sample=$2
 read_length=$3
