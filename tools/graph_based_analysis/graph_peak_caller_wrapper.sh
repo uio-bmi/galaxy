bc424e13686ab69022c2a75e7a73da3012d50c4d
Ivar Grytten <ivargry@invivo.hpc.uio.no>
2018-03-19 20:35:07 +0100

    Dev graph peak caller tool

--- tools/graph_based_analysis/graph_peak_caller_wrapper.sh
+++ tools/graph_based_analysis/graph_peak_caller_wrapper.sh
@@ -33,6 +33,10 @@ if [ $genome = "drosophila_melanogaster" ]; then
     #chromosomes="chrX,chr4"
 elif [ $genome = "arabidopsis_thaliana" ] ; then
     genome_size="135000000"
+    chromosomes="1,2,3,4,5,6"
+elif [ $genome == "yeast" ] ; then
+    genome_size="12100000"
+    chromosomes="1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16"
 else
     >&2 echo "Error: Invalid genome."
     exit 1
@@ -47,10 +51,10 @@ echo "Found $unique_reads unique control reads."
 echo "Has control $has_control"
 
 echo "Splitting reads"
-/usit/abel/u1/ivargry/.conda/envs/py36/bin/python3.6 /usit/abel/u1/ivargry/graph_peak_caller/graph_peak_caller/command_line_interface.py split_vg_json_reads_into_chromosomes $chromosomes $sample $graph_dir > split_log.txt 2>& 1
+/usit/abel/u1/ivargry/.conda/envs/py36/bin/python3.6 /usit/abel/u1/ivargry/graph_peak_caller/graph_peak_caller/command_line_interface.py split_vg_json_reads_into_chromosomes $chromosomes $sample $graph_dir
 
 if [ $sample != $control ]; then
-    /usit/abel/u1/ivargry/.conda/envs/py36/bin/python3.6 /usit/abel/u1/ivargry/graph_peak_caller/graph_peak_caller/command_line_interface.py split_vg_json_reads_into_chromosomes $chromosomes $control $graph_dir > split_log_control.txt 2>& 1
+    /usit/abel/u1/ivargry/.conda/envs/py36/bin/python3.6 /usit/abel/u1/ivargry/graph_peak_caller/graph_peak_caller/command_line_interface.py split_vg_json_reads_into_chromosomes $chromosomes $control $graph_dir
 fi
 
 
@@ -68,7 +72,7 @@ for chromosome in $(echo $chromosomes | tr "," "\n")
 do
     /usit/abel/u1/ivargry/.conda/envs/py36/bin/python3.6 /usit/abel/u1/ivargry/graph_peak_caller/graph_peak_caller/command_line_interface.py callpeaks_whole_genome $chromosome -d $graph_dir/  \
             -s "${sample_base_name}_" \
-            -f $fragment_length -r $read_length -p True -u $unique_reads -g $genome_size > log_before_pvalues_chr$chromosome.txt #2>&1 &
+            -f $fragment_length -r $read_length -p True -u $unique_reads -g $genome_size
 done
 
 wait
@@ -76,12 +80,12 @@ wait
 for chromosome in $(echo $chromosomes | tr "," "\n")
 do
 	/usit/abel/u1/ivargry/.conda/envs/py36/bin/python3.6 /usit/abel/u1/ivargry/graph_peak_caller/graph_peak_caller/command_line_interface.py callpeaks_whole_genome_from_p_values \
-                $chromosome -d $graph_dir -f $fragment_length -r $read_length > log_after_pvalues_chr$chromosome.txt #2>&1&
+                $chromosome -d $graph_dir -f $fragment_length -r $read_length
 done
 
 wait
 
-/usit/abel/u1/ivargry/.conda/envs/py36/bin/python3.6 /usit/abel/u1/ivargry/graph_peak_caller/graph_peak_caller/command_line_interface.py concatenate_sequence_files $chromosomes $out_fasta_file  > log_concatenating.txt 2>&1
+/usit/abel/u1/ivargry/.conda/envs/py36/bin/python3.6 /usit/abel/u1/ivargry/graph_peak_caller/graph_peak_caller/command_line_interface.py concatenate_sequence_files $chromosomes $out_fasta_file
 cat *_max_paths.intervalcollection >> $out_json_file
 
 # Get linear peaks
@@ -89,7 +93,7 @@ cat *_max_paths.intervalcollection >> $out_json_file
 for chromosome in $(echo $chromosomes | tr "," "\n")
 do
     /usit/abel/u1/ivargry/.conda/envs/py36/bin/python3.6 /usit/abel/u1/ivargry/graph_peak_caller/graph_peak_caller/command_line_interface.py \
-        peaks_to_linear ${chromosome}_max_paths.intervalcollection $graph_dir/${chromosome}_linear_pathv2.interval $chromosome linear_peaks_$chromosome.bed > diff_expressed_log.txt 2>&1
+        peaks_to_linear ${chromosome}_max_paths.intervalcollection $graph_dir/${chromosome}_linear_pathv2.interval $chromosome linear_peaks_$chromosome.bed
 done
 
 cat linear_peaks_*.bed >> $out_linear_peaks_file
@@ -111,7 +115,7 @@ else
     for chromosome in $(echo $chromosomes | tr "," "\n")
     do
         /usit/abel/u1/ivargry/.conda/envs/py36/bin/python3.6 /usit/abel/u1/ivargry/graph_peak_caller/graph_peak_caller/command_line_interface.py diffexpr \
-            $chromosome $graph_dir/$chromosome.nobg fimo_$chromosome/fimo.txt $graph_dir/$chromosome.json > diffexpr_log.txt 2>&1
+            -g $graph_dir/$chromosome.nobg $chromosome fimo_$chromosome/fimo.txt
 
         cat ${chromosome}_diffexpr.fasta >> $out_differentially_expressed
     done
